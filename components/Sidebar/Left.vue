a757168a532fe0f52ecd9b9fd613d780be13cb13
Tanise Williams <tanise.williams@icloud.com>
2023-02-25 13:30:44 +0000

    first commit

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <div class="p-2 my-2 hover:bg-blue-50 hover:rounded-full w-min dark:hover:bg-white/20" :class="transition">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter/>
+                </div>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const transition = 'transition ease-in-out duration-350'
+</script>
